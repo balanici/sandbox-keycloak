9541f20c843cd6a61431dc9efd2accb1a6501061
Igor Balanici <igor.balanici@gmail.com>
2023-07-24 17:09:52 +0300

    Initial commit

---
+++ src/main/java/com/balanici/sandboxkeycloak/SandboxKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.balanici.sandboxkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxKeycloakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/balanici/sandboxkeycloak/SandboxKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balanici.sandboxkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
